4c6e7cb676a58f0f61faf8c7a5883261bf9852e4
EdwinSan <jaret-@live,com.mx>
2022-04-20 19:56:32 -0500

    Adding the files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { NavBar } from "./NavBar";
+import "./NavBar.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(  <NavBar />);
+
